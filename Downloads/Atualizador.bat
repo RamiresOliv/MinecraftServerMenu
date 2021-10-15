a60b1053a19620b49e30f8d1b0cfca1a1faabcee
Gabriel Ramires de Oliveira <69019082+gabrielramires@users.noreply.github.com>
2021-10-15 20:19:10 -0300

    Add files via upload

---
+++ Downloads/Atualizador.bat
@@ -0,0 +1,78 @@
+@echo off
+SET mypath=%~dp0
+
+for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
+  set "DEL=%%a"
+)
+
+:comecaratualizacao
+title Minecraft Server Menu Instaler Updating...
+cls
+echo porfavor espere estamos fazendo download da atualizacao...
+timeout /t 2 /nobreak > nul
+attrib -H versionidlocal.txt
+del "versionidlocal.txt"
+del "Server.bat"
+del "Minecraft_Server_Menu_Installer.bat"
+del "LICENSE"
+cls
+call :colorEcho 03 "[LOGS]"
+echo :
+call :colorEcho 0e "[EM FILA] "
+echo : iniciando download de Minecraft Server Menu Instaler...
+bitsadmin.exe /transfer "Minecraft Server Menu Instaler" https://serverminecraftmenu.ramiresoliv.repl.co/Downloads/Installers/Minecraft_Server_Menu_Installer.bat %mypath:~0,-1%\Minecraft_Server_Menu_Installer.bat > nul
+if exist Minecraft_Server_Menu_Installer.bat (
+call :colorEcho 0a "[DONE] "
+echo Minecraft Server Menu Instaler instalado
+) else (
+call :colorEcho 04 "[FATAL ERROR] "
+echo : Ocorreu um erro ao instalar a Minecraft_Server_Menu_Installer.bat tente novamente mais tarde! 
+)
+call :colorEcho 0e "[EM FILA] "
+echo : iniciando download de LICENSE
+bitsadmin.exe /transfer "LICENSE" https://serverminecraftmenu.ramiresoliv.repl.co/LICENSE.txt %mypath:~0,-1%\LICENSE > nul
+if exist LICENSE (
+call :colorEcho 0a "[DONE] "
+echo LICENSE instalado
+) else (
+call :colorEcho 04 "[FATAL ERROR] "
+echo : Ocorreu um erro ao instalar a LICENSE tente novamente mais tarde! 
+)
+call :colorEcho 0e "[EM FILA] "
+echo : iniciando download de pacote de versoes...
+bitsadmin.exe /transfer "Menu id" https://serverminecraftmenu.ramiresoliv.repl.co/version.txt %mypath:~0,-1%\versionidlocal.txt > nul
+if exist versionidlocal.txt (
+attrib +H versionidlocal.txt
+call :colorEcho 0a "[DONE] "
+echo pacote de versoes instalado
+) else (
+call :colorEcho 04 "[FATAL ERROR] "
+echo : Ocorreu um erro ao instalar a versionidlocal.txt tente novamente mais tarde! 
+)
+call :colorEcho 0e "[EM FILA] "
+echo : iniciando download de Server.bat
+bitsadmin.exe /transfer "Server.bat" https://serverminecraftmenu.ramiresoliv.repl.co/Downloads/Minecraft_Java_Server.bat %mypath:~0,-1%\Server.bat > nul
+if exist Server.bat (
+call :colorEcho 0a "[DONE] "
+echo Server.bat instalado
+) else ( 
+call :colorEcho 04 "[FATAL ERROR] "
+echo : Ocorreu um erro ao instalar a Server.bat tente novamente mais tarde! 
+)
+echo [HTTPS]: conectando...
+title Minecraft Server Menu Instaler Update Ended
+timeout /t 2 /nobreak > nul
+call :colorEcho 03 "[LOG] "
+echo : Atualizacao terminou.
+call :colorEcho 0a "[ENDED] "
+echo : Ended.
+pause > nul
+cls
+start Minecraft_Server_Menu_Installer.bat
+exit
+
+:colorEcho
+echo off
+<nul set /p ".=%DEL%" > "%~2"
+findstr /v /a:%1 /R "^$" "%~2" nul
+del "%~2" > nul 2>&1i
