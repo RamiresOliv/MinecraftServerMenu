f96422efb16902d3fdcb2cb26dcdde66cc4d4a99
gabrielramires <gabrielramiresdeoliveira1@gmail.com>
2021-10-20 14:11:37 -0300

    f

--- Publish.bat
+++ Publish.bat
@@ -6,25 +6,49 @@
 :: NEED USE "git init" BEFORE USE THE BATCH
 
 @echo off
-if exist ".git" (
+
+echo [Copyright Ramires Oliv]
+
+if exist ".git" ( echo ... ) else (
+    echo Not Success.
+    echo Result: Error in try use git. pls verify if "git init" was used.
+    exit
+)
+
+echo pull           Incorporates changes from a remote repository into the current branch. In its default mode
+echo publish        (AUTO PUBLISH) add, commit, pull to local branch
+echo add            (MANUAL PUBLISH) This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit.
+echo commit         (MANUAL PUBLISH) Updates remote refs using local refs, while sending objects necessary to complete the given refs.
+
+set /p select =
+
+if %select% GEQ pull goto pull
+
+:pull
+    title pull
+    git pull
+    echo Success!
+    echo Result: pulled to GitHub Local Branch.
+
+:add
     title add
-    cls
+    git add -A
+    echo Success!
+    echo Result: added local Branch.
+
+
+:publish
+    title add
     git add -A
     title commit
     :: change here the commit name the normal name its "Cool Commit"
     git commit -m "Cool Commit"
     title push
     git push
-    cls
     title %CD% - cmd
-    cls
     echo [Copyright Ramires Oliv]
     echo Success!
-    echo Result: added, commited and pushed to local Branch.
-) else (
-    title %CD% - cmd
-    cls
-    echo [Copyright Ramires Oliv]
-    echo Not Success.
-    echo Result: Error in try use git. pls verify if "git init" was used.
-)
+    echo Result: added, commited and pushed to GitHub local Branch.
+
+
+echo ...
