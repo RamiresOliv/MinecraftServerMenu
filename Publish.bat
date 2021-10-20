8e2b646c23c5b2445253a5e1ebf0cd1cfeb30d76
gabrielramires <gabrielramiresdeoliveira1@gmail.com>
2021-10-20 15:35:49 -0300

    Cool Commit

--- Publish.bat
+++ Publish.bat
@@ -15,14 +15,16 @@ if exist ".git" ( echo ... ) else (
     exit
 )
 
-echo pull           Incorporates changes from a remote repository into the current branch. In its default mode
-echo publish        (AUTO PUBLISH) add, commit, pull to local branch
-echo add            (MANUAL PUBLISH) This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit.
-echo commit         (MANUAL PUBLISH) Updates remote refs using local refs, while sending objects necessary to complete the given refs.
-echo push         (MANUAL PUBLISH) Updates remote refs using local refs, while sending objects necessary to complete the given refs.
+echo pull       Incorporates changes from a remote repository into the current branch. In its default mode
+echo publish    (AUTO PUBLISH) add, commit, pull to local branch
+echo add        (MANUAL PUBLISH) This command updates the index using the current content found in the working tree, to prepare the content staged for the next commit.
+echo commit     (MANUAL PUBLISH) Updates remote refs using local refs, while sending objects necessary to complete the given refs.
+echo push       (MANUAL PUBLISH) Updates remote refs using local refs, while sending objects necessary to complete the given refs.
+echo leave/exit (HELPER COMMAND) Leave of the Git Helper
 goto main
 
 :main
+echo.
 title %CD% running git helper by RamiresOliv :D
 
 set /p select=
@@ -32,6 +34,7 @@ if %select% equ push goto push
 if %select% equ add goto add
 if %select% equ commit goto commit
 if %select% equ publish goto publish
+if %select% equ leave goto leave
 
 if %select% GEQ 0 goto Error
 
@@ -45,7 +48,7 @@ if %select% GEQ 0 goto Error
     git pull
     echo Success!
     echo Result: pushed to GitHub Local Branch.
-    exit
+    goto main
 
 :pull
     title %CD% running git pull...
@@ -53,21 +56,21 @@ if %select% GEQ 0 goto Error
     git pull
     echo Success!
     echo Result: pulled to GitHub Local Branch.
-    exit
+    goto main
 
 :add
     title %CD% running git add -A...
     git add -A
     echo Success!
     echo Result: added local Branch.
-    exit
+    goto main
 
 :commit
     title %CD% running git commit -m "Cool Commit"...
     git commit -m "Cool Commit"
     echo Success!
     echo Result: commit local Branch.
-    exit
+    goto main
 
 :publish
     title %CD% running git add -A...
@@ -81,6 +84,11 @@ if %select% GEQ 0 goto Error
     echo [Copyright Ramires Oliv]
     echo Success!
     echo Result: added, commited and pushed to GitHub local Branch.
-    exit
+    goto main
 
+:leave
+echo.
+title %CD% - cmd
 echo ...
+echo.
+@echo on
