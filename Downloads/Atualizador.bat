c74c1a0e98c99c2f874666b99e23fd4e757e2921
gabrielramires <gabrielramiresdeoliveira1@gmail.com>
2021-11-02 23:40:00 -0300

    Cool Commit

--- Downloads/Atualizador.bat
+++ Downloads/Atualizador.bat
@@ -1,5 +1,5 @@
 @echo off
-SET mypath=%~dp0
+SET mypath=%cd%
 
 for /F "tokens=1,2 delims=#" %%a in ('"prompt #$H#$E# & echo on & for %%b in (1) do     rem"') do (
   set "DEL=%%a"
@@ -32,7 +32,7 @@ echo : Ocorreu um erro ao instalar a Minecraft_Server_Menu_Installer.bat tente n
 
 call :colorEcho 0e "[EM FILA] "
 echo : iniciando download de LICENSE
-bitsadmin.exe /transfer "LICENSE" https://raw.githubusercontent.com/gabrielramires/MinecraftServerMenu/Files/LICENSE.txt %cd%\LICENSE > nul
+bitsadmin.exe /transfer "LICENSE" https://raw.githubusercontent.com/gabrielramires/MinecraftServerMenu/Files/LICENSE.txt %mypath%\LICENSE > nul
 if exist LICENSE (
 call :colorEcho 0a "[DONE] "
 echo LICENSE instalado
@@ -42,7 +42,7 @@ echo : Ocorreu um erro ao instalar a LICENSE tente novamente mais tarde!
 )
 call :colorEcho 0e "[EM FILA] "
 echo : iniciando download de pacote de versoes...
-bitsadmin.exe /transfer "Menu id" https://raw.githubusercontent.com/gabrielramires/MinecraftServerMenu/Files/version.txt %cd%\versionidlocal.txt > nul
+bitsadmin.exe /transfer "Menu id" https://raw.githubusercontent.com/gabrielramires/MinecraftServerMenu/Files/version.txt %mypath%\versionidlocal.txt > nul
 if exist versionidlocal.txt (
 attrib +H versionidlocal.txt
 call :colorEcho 0a "[DONE] "
@@ -54,7 +54,7 @@ echo : Ocorreu um erro ao instalar a versionidlocal.txt tente novamente mais tar
 
 call :colorEcho 0e "[EM FILA] "
 echo : iniciando download de Server.bat
-bitsadmin.exe /transfer "Server.bat" https://raw.githubusercontent.com/gabrielramires/Assets/Website/WebSiteStorage/Downloads/Minecraft_Java_Server.bat %cd%\Server.bat > nul
+bitsadmin.exe /transfer "Server.bat" https://raw.githubusercontent.com/gabrielramires/Assets/Website/WebSiteStorage/Downloads/Minecraft_Java_Server.bat %mypath%\Server.bat > nul
 if exist Server.bat (
 call :colorEcho 0a "[DONE] "
 echo Server.bat instalado
